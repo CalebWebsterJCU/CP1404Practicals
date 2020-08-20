89840964630d03183bb19f11e164f95013d71014
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-20 16:53:38 +1000

    Change password checker to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,11 +1,24 @@
 """This program gets a password from the user and prints asterisks as long as the password."""
+
 MINIMUM_LENGTH = 5
-password = input("Enter password: ")
-length = len(password)
-while length < 5:
-    print("Invalid password")
+
+
+def main():
+
+    password = get_password()
+    print_password_asterisks(password)
+
+
+def print_password_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter password: ")
-    length = len(password)
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid password")
+        password = input("Enter password: ")
+    return password
 
-print("*" * length)
 
+main()
