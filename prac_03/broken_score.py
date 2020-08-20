0cebe57156630dd35864e6fb9cfa5689795bd3db
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-20 21:28:51 +1000

    Extract get_valid_score,  determine_score_category, and run_tests functions.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,17 +5,36 @@ Broken program to determine score status
 
 
 def main():
-    """Determine category based on grade."""
+    """Get score and determine and print category."""
+    score = get_valid_score(minimum=0, maximum=100)
+    category = determine_score_category(score=score)
+    print(category)
+
+
+def get_valid_score(minimum: int, maximum: int) -> float:
+    """Get a valid score between minimum and maximum values."""
     score = float(input("Enter score: "))
-    if 0 <= score <= 100:
-        if score >= 90:
-            print("Excellent")
-        elif score >= 50:
-            print("Passable")
-        else:
-            print("Bad")
-    else:
+    while score < minimum or score > maximum:
         print("Invalid Score")
+        score = float(input("Enter score: "))
+    return score
 
 
-main()
+def determine_score_category(score: float) -> str:
+    """Determine category based on grade."""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def run_tests():
+    """Test determine_score function."""
+    for x in range(0, 101, 10):
+        print("{} - {}".format(x, determine_score_category(x)))
+
+
+run_tests()
+# main()
