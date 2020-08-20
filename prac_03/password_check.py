b2827bcc64c4d7f57b8979e1f93e57c751e21da0
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-20 20:55:56 +1000

    Add function docstrings

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -4,18 +4,21 @@ MINIMUM_LENGTH = 5
 
 
 def main():
+    """Get password, then print asterisks."""
 
-    password = get_password()
+    password = get_password(MINIMUM_LENGTH)
     print_password_asterisks(password)
 
 
 def print_password_asterisks(password):
+    """Print asterisks based on length of password."""
     print("*" * len(password))
 
 
-def get_password():
+def get_password(min_length):
+    """Get password, making sure that it is longer than minimum length."""
     password = input("Enter password: ")
-    while len(password) < MINIMUM_LENGTH:
+    while len(password) < min_length:
         print("Invalid password")
         password = input("Enter password: ")
     return password
