43b8413a874164067bb23ee317c2284e96d39004
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-20 21:53:52 +1000

    Add scores.py, results.txt

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -1,4 +1,25 @@
-"""This program gets a number of scores, generates that number of random scores between minimum and maximum values, """
+"""This program gets a number of scores, generates that number of random scores
+between minimum and maximum values, and writes each result to a text file."""
+
+import random as r
+
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+FILE_NAME = 'results.txt'
+
+
+def main():
+    """Get number of scores, generate random scores, write categories to text file."""
+    number_of_scores = get_positive_integer('Number of scores: ')
+    file_out = open(FILE_NAME, 'w')
+    for x in range(number_of_scores):
+        score = r.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+        category = determine_score_category(score)
+        print("{} - {}".format(score, category), file=file_out)
+    file_out.close()
+    print("Wrote {} results to '{}'".format(number_of_scores, FILE_NAME))
+
+
 def determine_score_category(score: float) -> str:
     """Determine category based on grade."""
     if score >= 90:
@@ -7,3 +28,25 @@ def determine_score_category(score: float) -> str:
         return "Passable"
     else:
         return "Bad"
+
+
+def get_positive_integer(prompt: str = 'Number: ', error: str = 'Please enter a positive integer'):
+    """Get a positive integer input from the user."""
+    is_valid = False
+    number = 0
+    while not is_valid:
+        try:
+            number = int(input(prompt))
+            is_valid = True
+        except ValueError:
+            print(error)
+    return number
+
+
+def run_tests():
+    """Test functions."""
+    print(get_positive_integer())
+
+
+# run_tests()
+main()
