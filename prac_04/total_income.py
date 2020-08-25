656e8f2409f07bcbb763087b1ec63d743680da1b
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-25 18:42:34 +1000

    Change total_income.py to use list comprehension

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -6,13 +6,13 @@ Starter code for cumulative total income program
 
 def main():
     """Display income report for incomes over a given number of months."""
-    incomes = []
     number_of_months = int(input("How many months? "))
+    incomes = [float(input("Enter income for month {}: ".format(month))) for month in range(1, number_of_months + 1)]
 
-    for month in range(1, number_of_months + 1):
-        # income = float(input("Enter income for month " + str(month) + ": "))
-        income = float(input("Enter income for month {}: ".format(month)))
-        incomes.append(income)
+    # for month in range(1, number_of_months + 1):
+    #     # income = float(input("Enter income for month " + str(month) + ": "))
+    #     income = float(input("Enter income for month {}: ".format(month)))
+    #     incomes.append(income)
 
     display_report(incomes, number_of_months)
 
