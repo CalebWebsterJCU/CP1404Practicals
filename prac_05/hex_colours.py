da0f00f4779c6fd4d64cfa10f85436e397baf15c
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-08-31 17:30:06 +1000

    Complete hex_colours.py Modify program to read hex values and names from colour_hex_values.csv

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,22 @@
+"""
+Hex Colours - This program shows different colour names and their hexidecimal codes.
+Data Retrieved from https://www.color-hex.com/color-names.html.
+"""
+
+FILE_NAME = "colour_hex_values.csv"
+
+name_to_hex = {}
+file_in = open(FILE_NAME, 'r')
+for line in file_in:
+    colour_name, colour_hex = line.strip().split(" ")
+    name_to_hex[colour_name] = colour_hex
+file_in.close()
+
+name = input("Colour Name: ")
+while name != "":
+    try:
+        print(f"The hex value for {name} is {name_to_hex[name]}")
+    except KeyError:
+        print("Invalid input")
+    name = input("Colour Name: ")
+print("Goodbye!")
