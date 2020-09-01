dd023c6567b85f435039fc17f37a9d767d0dfddf
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-01 19:26:53 +1000

    Add __str__ method to ProgrammingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,6 +6,10 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        """Return printing instructions."""
+        return f"{self.name}, {self.typing}, typing, Reflection={self.reflection}, First appeared in {self.year}"
+
     def is_dynamic(self):
         """Check if a programming language is dynamic or not."""
         if self.reflection == "Dynamic":
