d434a26605c4fa8b9759ab6d8ea0ec547061335f
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-17 13:37:08 +1000

    Change GUI Layout to add clear button with clear_input() function Add handle_convert on change mode Set window size fix rounding on fahrenheit output add clear_input() function

--- prac_07/convert_temperature.py
+++ prac_07/convert_temperature.py
@@ -9,21 +9,23 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.core.window import Window
 
+C_TO_F_MODE = "c-f"
+F_TO_C_MODE = "f-c"
+
 
 class ConvertTemperature(App):
     """Create GUI Program."""
     temp_output_text = StringProperty()
     mode_text = StringProperty()
-    C_TO_F_MODE = "c-f"
-    F_TO_C_MODE = "f-c"
 
     def __init__(self, **kwargs):
         """Initialize class."""
         super().__init__(**kwargs)
-        self.mode = self.F_TO_C_MODE
+        self.mode = F_TO_C_MODE
 
     def build(self):
         """Create GUI."""
+        Window.size = (600, 300)
         self.root = Builder.load_file("convert_temperature.kv")
         self.temp_output_text = "Enter Temperature"
         self.mode_text = "Fahrenheit to Celsius"
@@ -32,10 +34,10 @@ class ConvertTemperature(App):
     def handle_convert(self):
         """Convert fahrenheit to celsius or celsius to fahrenheit, depending on which mode the program is in."""
         temp_to_convert = self.get_valid_temperature()
-        if self.mode == self.F_TO_C_MODE:  # Fahrenheit to celsius
-            self.temp_output_text = str(round((temp_to_convert - 32) * (5 / 9), 4))
+        if self.mode == F_TO_C_MODE:  # Fahrenheit to celsius
+            self.temp_output_text = str(round((temp_to_convert - 32) * (5 / 9), 4)) + "°C"
         else:  # celsius to fahrenheit
-            self.temp_output_text = str(round(temp_to_convert * (9 / 5) + 32))
+            self.temp_output_text = str(round(temp_to_convert * (9 / 5) + 32, 4)) + "°F"
 
     def get_valid_temperature(self):
         """Get temperature, returning zero if it is invalid."""
@@ -47,12 +49,16 @@ class ConvertTemperature(App):
 
     def change_mode(self):
         """Change program mode (celsius to fahrenheit or fahrenheit to celsius)."""
-        if self.mode == self.F_TO_C_MODE:
-            self.mode = self.C_TO_F_MODE
+        if self.mode == F_TO_C_MODE:
+            self.mode = C_TO_F_MODE
             self.mode_text = "Celsius to Fahrenheit"
         else:
-            self.mode = self.F_TO_C_MODE
+            self.mode = F_TO_C_MODE
             self.mode_text = "Fahrenheit to Celsius"
+
+    def clear_input(self):
+        """Clear temperature input field."""
+        self.root.ids.temp_input.text = ""
 
 
 if __name__ == '__main__':
