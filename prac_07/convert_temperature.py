94ed40e5e91804b74752c00d5bbcb4d987154eda
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-17 12:41:10 +1000

    Complete program functionality Add build, handle_convert, get_valid_temperature, and change_mode functions Add functionality for changing between fahrenheit to celsius and celsius to fahrenheit modes

---
+++ prac_07/convert_temperature.py
@@ -0,0 +1,57 @@
+"""
+Convert Temperature
+17/09/2020
+This Kivy Program Converts celsius to fahrenheit and fahrenheit to celsius.
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.core.window import Window
+
+C_TO_F_MODE = "c-f"
+F_TO_C_MODE = "f-c"
+
+
+class ConvertTemperature(App):
+    """Create GUI Program."""
+    temp_output = StringProperty
+
+    def __init__(self, **kwargs):
+        """Initialize class."""
+        super().__init__(**kwargs)
+        self.mode = F_TO_C_MODE
+
+    def build(self):
+        """Create GUI."""
+        self.root = Builder.load_file("convert_temperature.kv")
+        return self.root
+
+    def handle_convert(self):
+        """Convert fahrenheit to celsius or celsius to fahrenheit, depending on which mode the program is in."""
+        temp_to_convert = self.get_valid_temperature()
+        if self.mode == F_TO_C_MODE:  # Fahrenheit to celsius
+            temp_output = round((temp_to_convert - 32) * (5 / 9), 4)
+        else:  # celsius to fahrenheit
+            temp_output = round(temp_to_convert * (9 / 5) + 32)
+
+    def get_valid_temperature(self):
+        """Get temperature, returning zero if it is invalid."""
+        try:
+            temperature = float(self.root.ids.temp_input.text)
+            return temperature
+        except ValueError:
+            return 0
+
+    def change_mode(self):
+        """Change program mode (celsius to fahrenheit or fahrenheit to celsius)."""
+        if self.mode == F_TO_C_MODE:
+            self.mode = C_TO_F_MODE
+            self.root.ids.change_mode_button.text = C_TO_F_MODE
+        else:
+            self.mode = F_TO_C_MODE
+            self.root.ids.change_mode_button.text = F_TO_C_MODE
+
+
+if __name__ == '__main__':
+    ConvertTemperature().run()
