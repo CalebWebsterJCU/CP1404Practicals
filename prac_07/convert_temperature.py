c78e781fe16246ebfaf8db523eccff711ae7d58f
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-17 13:21:09 +1000

    Bind handle_convert() function to temp_input on_text Fix: change handle_convert() to change_mode() Fix various errors

--- prac_07/convert_temperature.py
+++ prac_07/convert_temperature.py
@@ -9,32 +9,33 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 from kivy.core.window import Window
 
-C_TO_F_MODE = "c-f"
-F_TO_C_MODE = "f-c"
-
 
 class ConvertTemperature(App):
     """Create GUI Program."""
-    temp_output_text = StringProperty
-    mode_text = StringProperty
+    temp_output_text = StringProperty()
+    mode_text = StringProperty()
+    C_TO_F_MODE = "c-f"
+    F_TO_C_MODE = "f-c"
 
     def __init__(self, **kwargs):
         """Initialize class."""
         super().__init__(**kwargs)
-        self.mode = F_TO_C_MODE
+        self.mode = self.F_TO_C_MODE
 
     def build(self):
         """Create GUI."""
         self.root = Builder.load_file("convert_temperature.kv")
+        self.temp_output_text = "Enter Temperature"
+        self.mode_text = "Fahrenheit to Celsius"
         return self.root
 
     def handle_convert(self):
         """Convert fahrenheit to celsius or celsius to fahrenheit, depending on which mode the program is in."""
         temp_to_convert = self.get_valid_temperature()
-        if self.mode == F_TO_C_MODE:  # Fahrenheit to celsius
-            temp_output = str(round((temp_to_convert - 32) * (5 / 9), 4))
+        if self.mode == self.F_TO_C_MODE:  # Fahrenheit to celsius
+            self.temp_output_text = str(round((temp_to_convert - 32) * (5 / 9), 4))
         else:  # celsius to fahrenheit
-            temp_output = str(round(temp_to_convert * (9 / 5) + 32))
+            self.temp_output_text = str(round(temp_to_convert * (9 / 5) + 32))
 
     def get_valid_temperature(self):
         """Get temperature, returning zero if it is invalid."""
@@ -46,12 +47,12 @@ class ConvertTemperature(App):
 
     def change_mode(self):
         """Change program mode (celsius to fahrenheit or fahrenheit to celsius)."""
-        if self.mode == F_TO_C_MODE:
-            self.mode = C_TO_F_MODE
-            mode_text = C_TO_F_MODE
+        if self.mode == self.F_TO_C_MODE:
+            self.mode = self.C_TO_F_MODE
+            self.mode_text = "Celsius to Fahrenheit"
         else:
-            self.mode = F_TO_C_MODE
-            mode_text = F_TO_C_MODE
+            self.mode = self.F_TO_C_MODE
+            self.mode_text = "Fahrenheit to Celsius"
 
 
 if __name__ == '__main__':
