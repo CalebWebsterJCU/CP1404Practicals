8f5acf269912bb3ce935eafef50407fc4a12ba66
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-17 11:26:58 +1000

    Change font sizes Change window size change grade_output string property to grade_output_text so as not to be confused with grade_output id

--- prac_07/grade.py
+++ prac_07/grade.py
@@ -7,6 +7,7 @@ This GUI Program takes a users score and displays their grade.
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
+from kivy.core.window import Window
 
 
 def determine_grade(score):
@@ -19,18 +20,24 @@ def determine_grade(score):
 
 class Grade(App):
     """Construct main app."""
-    grade_output = StringProperty()
+    grade_output_text = StringProperty()
 
     def build(self):
         """Build the GUI."""
         self.root = Builder.load_file("grade.kv")
+        Window.size = (600, 300)
+        self.grade_output_text = "Enter Score"
         # self.grade_output = "Enter Score"
         return self.root
 
     def handle_display_grade(self):
         score = self.get_valid_score()
         grade = determine_grade(score)
-        self.grade_output = grade
+        self.grade_output_text = grade
+        if grade == "Fail":
+            self.root.ids.grade_output.color = (1, 0, 0, 1)
+        else:
+            self.root.ids.grade_output.color = (0, 1, 0, 1)
 
     def get_valid_score(self):
         """Get score, returning zero if it is invalid."""
@@ -43,7 +50,7 @@ class Grade(App):
     def clear_input_output(self):
         """Clear input and output boxes."""
         self.root.ids.score_input.text = ""
-        self.grade_output = ""
+        self.grade_output_text = ""
 
 
 if __name__ == '__main__':
