b0b95bbb183a378ebe14dc83b89a41f8ac09aa59
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-25 17:45:53 +1000

    Change program to create class objects instead of tuples sort list of guitars using __lt__ method defined in class

--- prac_08/guitars_file_reader_v2.py
+++ prac_08/guitars_file_reader_v2.py
@@ -4,6 +4,8 @@ Guitars File Reader
 This file reads information from guitars.csv and displays the guitars inside.
 """
 
+from prac_06.guitar import Guitar
+
 FILE_NAME = "guitars.csv"
 
 
@@ -14,9 +16,11 @@ def main():
     for line in file_in:
         parts = line.strip().split(",")
         parts[1], parts[2] = int(parts[1]), float(parts[2])
-        guitars.append(tuple(parts))
+        guitars.append(Guitar(parts[0], parts[1], parts[2]))
     file_in.close()
-    print(guitars)
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
 
 
 main()
