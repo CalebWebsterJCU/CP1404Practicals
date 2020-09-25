9202d7745e70c8d152bee3c1751be5c9ea66fb0d
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-25 17:31:41 +1000

    Complete main() program program reads file, splits lines into parts, converts numerical elements to ints and floats, appends tuple of parts to list, then displays list of guitars

---
+++ prac_08/guitars_file_reader.py
@@ -0,0 +1,23 @@
+"""
+Guitars File Reader
+25/09/2020
+This file reads information from guitars.csv and displays the guitars inside.
+"""
+
+FILE_NAME = "guitars.csv"
+
+
+def main():
+    """Read file and display contents as a list of tuples."""
+    guitars = []
+    file_in = open(FILE_NAME, "r")
+    for line in file_in:
+        parts = line.strip().split(",")
+        parts[1] = int(parts[1])
+        parts[2] = float(parts[2])
+        guitars.append(tuple(parts))
+    file_in.close()
+    print(guitars)
+
+
+main()
