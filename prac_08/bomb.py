cad468f2260109bdb6bb5ed40c8d400bb67021d0
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-09-26 12:16:17 +1000

    Complete Bomb(Car) class  - change drive() to add 0 to odometer  - change __str__ to print "Bomb"  - add push_distance class variable  - add push() function, which moves the car    push_distance units without consuming fuel

--- prac_08/bomb.py
+++ prac_08/bomb.py
@@ -7,4 +7,38 @@ This class inherits from Car and represents a defective car that doesn't drive, 
 from prac_08.car import Car
 
 
+class Bomb(Car):
+    """Crappy car that doesn't move when it drives."""
+    push_distance = 10
 
+    def __init__(self, name, fuel):
+        """Initialize Bomb class."""
+        super().__init__(name, fuel)
+
+    def __str__(self):
+        """Define rules for printing class objects."""
+        return super().__str__() + " (Bomb)"
+
+    def drive(self, distance):
+        """Drive the car a given distance, consuming fuel, but not increasing distance."""
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += 0
+        return distance
+
+    def push(self):
+        """Push the car a certain distance. Put ya back into it mate!"""
+        self.odometer += self.push_distance
+        return self.push_distance
+
+
+if __name__ == '__main__':
+    car = Bomb("Range Rover", 100)
+    print(car)
+    car.drive(100)
+    print(car)
+    car.push()
+    print(car)
