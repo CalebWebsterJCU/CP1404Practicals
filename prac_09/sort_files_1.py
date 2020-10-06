7e484df44a36b70881ae2a978dfd1243b6ba2f72
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-10-06 16:43:20 +1000

    Complete file sorting program Add functionality for scanning directory, getting extensions, creating directories and moving files. Add error checking with try loop to catch FileExistsError and shutil.Error Sort files in FilesToSort directory

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,4 +1,44 @@
 """
 Sort Files 1
-
+6/10/2020
+This program sorts the files in FilesToSort folder into new folders based on file type.
 """
+
+import os
+import shutil
+
+
+def main():
+    """Move files with the same file extension into a directory with the name of the extension."""
+    # Change to File directory
+    os.chdir("FilesToSort")
+
+    # Get a list of all the filenames
+    filenames = os.listdir('.')
+    directories_created = []
+    print("Files in {}:".format(os.getcwd()))
+    for filename in filenames:
+        print("\nFile Name: {}".format(filename))
+
+        # Find file extension
+        extension = filename[filename.find('.') + 1:]
+        print("Extension: {}".format(extension))
+
+        # If extension is not already a directory,
+        # create a directory with the name of the extension
+        try:
+            directory_name = extension
+            if directory_name not in directories_created:
+                directories_created.append(directory_name)
+                os.mkdir(directory_name)
+            print("{} moved to {}".format(filename, directory_name))
+
+            # Move all files into their respective directories
+            shutil.move(filename, "{}/".format(directory_name) + filename)
+        except FileExistsError:
+            pass
+        except shutil.Error:
+            pass
+
+
+main()
